102af0055c9edd41fe16e814dbbf9e82c96f3e37
gabriela <gabysiiu@hotmail.com>
2024-04-19 21:37:25 -0700

    local storage '[object HTMLInputElemen', button/link to blog

--- assets/js/form.js
+++ assets/js/form.js
@@ -34,6 +34,7 @@ const sumit = document.getElementById("mysumit");
 function func1(){
     userName.innerHTML = userInput.value;
     console.log(userName);
+    //localStorage.setItem( 'userN', userName);
 }
 
 sumit.addEventListener('click', func1);
@@ -49,3 +50,10 @@ function func3 (){
     console.log(content);
 }
 sumit.addEventListener('click', func3);
+$('#link').click();
+//document.getElementById('').click();
+
+
+localStorage.setItem('userName', userInput);
+localStorage.setItem('blogtitle', titleInput);
+localStorage.setItem('content', contInput);
