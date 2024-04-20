855207b1a2c6547386ed776700b0bfc4043ff0a0
gabriela <gabysiiu@hotmail.com>
2024-04-19 20:10:53 -0700

    const update/rename doc

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,31 @@
+//const userNinput = document.querySelector('.userN');
+//const titleInput = document.querySelector('.bTitle');
+//const contInput = document.querySelector('.cont');
+//const sumitInput = document.querySelector()
+//const submitEl = document.querySelector('#submit');
+
+//submitEl.addEventListener('click');
+// CUANDO HAGO ESTO SOLAMENTE ME SALE EL VALOR DEL ULTIMO 
+
+/*
+let userName; 
+document.getElementById("mysumit").onclick = function (){
+    userName = document.getElementById("userInput").value; 
+    console.log(userName);
+}
+
+let blogtitle; 
+document.getElementById("mysumit").onclick = function (){
+    blogtitle = document.getElementById("titleInput").value; 
+    console.log(blogtitle);
+}
+
+let content; 
+document.getElementById("mysumit").onclick = function (){
+    content = document.getElementById("contInput").value; 
+    console.log(content);
+}*/
+ 
 const userName = document.getElementById("userInput");
 const blogtitle = document.getElementById("titleInput");
 const content = document.getElementById("contInput");
@@ -11,13 +39,13 @@ function func1(){
 sumit.addEventListener('click', func1);
 
 function func2(){
-    blogtitle.innerHTML = titleInput.value;
+    blogtitle.innerHTML = blogtitle.value;
     console.log(blogtitle);
 }
 sumit.addEventListener('click', func2);
 
 function func3 (){
-    content.innerHTML = contInput.value;
+    content.innerHTML = content.value;
     console.log(content);
 }
 sumit.addEventListener('click', func3);
