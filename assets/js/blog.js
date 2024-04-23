74220b18293f24ab0a5be5b674d3780de50557a4
gabriela <gabysiiu@hotmail.com>
2024-04-22 19:03:53 -0700

    functions for the blog UPDATE

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,22 +1,51 @@
-const testTitle = document.getElementById("testTitle")
 
+const titleFromLocalStorage = localStorage.getItem("Title")
+const contentFromLocalStorage = localStorage.getItem("Content")
+const userFromLocalStorage = localStorage.getItem("Username")
+//turns page into dark-mode
 function myFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
- }
+}
+
+
+const posts = document.querySelector(".posts");
+
+function createElement() {
+    const blogcontainer = document.createElement('div');
+    blogcontainer.classList.add("blogcontainer");
+    posts.appendChild(blogcontainer);
+    //kuego agtegar los otros con los nombres del html (notas)
+    const title = document.createElement('h4');
+    title.classList.add("title");
+    title.textContent = titleFromLocalStorage;
+    blogcontainer.appendChild(title);
+}
+createElement();
 
 
 
- function displayUserName () {
-    const nameFromLocalStorage = localStorage.getItem("Title")
-    //const contentFromLocalStorage = localStorage.getItem("Content")
-    if (nameFromLocalStorage) {
-      testTitle.textContent = nameFromLocalStorage
-    } else {
-      testTitle.textContent = "No name data in local storage"
-    }
-  }
-  
-  displayUserName();
- 
-  
+function callcontent() {
+
+    const inputcontent = document.createElement('div');
+    inputcontent.classList.add("inputcontent");
+    posts.appendChild(inputcontent);
+    const inputc = document.createElement('p');
+    inputc.classList.add("content");
+    inputc.textContent = contentFromLocalStorage;
+    inputcontent.appendChild(inputc);
+}
+callcontent();
+
+
+function callusername() {
+
+    const username = document.createElement('div');
+    username.classList.add("username");
+    posts.appendChild(username);
+    const userN = document.createElement('p');
+    userN.classList.add("userN");
+    userN.textContent = userFromLocalStorage;
+    username.appendChild(userN);
+}
+callusername();
--- assets/js/form.js
+++ assets/js/form.js
@@ -4,84 +4,51 @@ let titles=[]
 let contents=[]
 
 
+//gets my value from the localStorage
 function getLocalStorage (){
-    
-    //userNames = JSON.parse(localStorage.getItem("Username"));
     if (!(localStorage.getItem("Username")=== null)){
         userNames = JSON.parse(localStorage.getItem("Username"));
     }
 }
 
+function getLocalStorage (){
+    if (!(localStorage.getItem("Title")=== null)){
+        titles = JSON.parse(localStorage.getItem("Title"));
+    }
+}
 
+function getLocalStorage (){
+    if (!(localStorage.getItem("Content")=== null)){
+        contents = JSON.parse(localStorage.getItem("Content"));
+    }
+}
+
+
+
+//turns my website into darkmode
 function myFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
  }
 
-
 const sumitBtn = document.getElementById('mysumit');
-
 sumitBtn.addEventListener('click', sumit); 
 
+//when clicking the mysumit button, the input submited will get storage in localstorage  
  function sumit(){
-
-
  const uInput = document.getElementById('userInput'); 
-userNames.push(uInput.value);
- //const userStorage = uInput.value;
+ userNames.push(uInput.value);
  localStorage.setItem('Username',JSON.stringify(userNames));
 
  const tInput = document.getElementById('titleInput');
- const titleStorage= tInput.value;
- localStorage.setItem( 'Title', titleStorage);
+ titles.push(tInput.value);
+ localStorage.setItem ('Title',JSON.stringify(titles));//( 'Title', titleStorage);
 
  const cInput = document.getElementById('usercont');
- const contStorage = cInput.value;
- localStorage.setItem('Content', contStorage);
-  
+ contents.push(cInput.value);
+ localStorage.setItem('Content', JSON.stringify(contents));
  }
 
- //lamando la función 
+ //calls the function
 getLocalStorage();
 
- /////hice esta función de abajo pero sigue sin aparecerme 
- // el valor
-
- //https://www.freecodecamp.org/news/use-local-storage-in-modern-applications/#:~:text=To%20store%20data%20in%20local%20storage%2C%20you%20use%20the%20setItem,a%20key%20and%20a%20value.&text=If%20the%20key%20does%20not,the%20given%20value%20to%20it.
-
-
- 
- 
- /*
-const userName = document.getElementById('userInput');
-const blogtitle = document.getElementById('titleInput');
-const content = document.getElementById('contInput');
-const sumit = document.getElementById("mysumit");
-const valueToStore = userName.value;
-
-function func1(){
-    userName.innerHTML = userInput.value;
-    console.log(userName);
-    //localStorage.setItem( 'userN', userName);
-}
-
-sumit.addEventListener('click', func1);
-
-function func2(){
-    blogtitle.innerHTML = blogtitle.value;
-    console.log(blogtitle);
-}
-sumit.addEventListener('click', func2);
-
-function func3 (){
-    content.innerHTML = content.value;
-    console.log(content);
-}
-sumit.addEventListener('click', func3);
-
-
-localStorage.setItem('userName', userInput);
-localStorage.setItem('blogtitle', titleInput);
-localStorage.setItem('content', contInput);
-
-*/
